9b66c885fba169c5634801c36857be06cd436f3f
Ryan Ansberry <ansman92@gmail.com>
2019-07-25 18:02:24 -0700

    File update

--- vr-funding/src/App.js
+++ vr-funding/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Register from './components/Register'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <Register />
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      projects: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://vrfp.herokuapp.com/')
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Register />
+      </div>
+    );
+  }
 }
-
-export default App;
--- vr-funding/src/components/Register.js
+++ vr-funding/src/components/Register.js
@@ -25,10 +25,10 @@ export default class Register extends React.Component {
                 <h1 className="register-header">Create new user</h1>
                 <div className="form-container">
                     <form className="register-form" onSubmit={this.submitHandler}>
-                        <input name="username" placeHolder="Username" value={this.state.username} onChange={this.changeHandler} /><br />
-                        <input name="password" placeHolder="Password" value={this.state.password} onChange={this.changeHandler} /><br />
-                        <input name="name" placeHolder="Name" value={this.state.name} onChange={this.changeHandler} /><br />
-                        <input name="about" placeHolder="About" value={this.state.about} onChange={this.changeHandler} /><br />
+                        <input name="username" placeholder="Username" value={this.state.username} onChange={this.changeHandler} /><br />
+                        <input name="password" placeholder="Password" value={this.state.password} onChange={this.changeHandler} /><br />
+                        <input name="name" placeholder="Name" value={this.state.name} onChange={this.changeHandler} /><br />
+                        <input name="about" placeholder="About" value={this.state.about} onChange={this.changeHandler} /><br />
                         <button type="submit">Submit</button>
                     </form>
                 </div>
