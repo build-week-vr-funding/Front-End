7cd83a90d0ca85006e5b45aa5dc20135f85200b7
RyanAnsberry <48343707+RyanAnsberry@users.noreply.github.com>
2019-08-02 18:33:19 -0700

    Removed faulty link

--- vr-funding/src/components/Navigation.js
+++ vr-funding/src/components/Navigation.js
@@ -14,7 +14,6 @@ export default function Navigation() {
                     <button className="login-btn nav-btn" >Log In</button>
                 </Link>
             </div>
-            <a href="https://www.vrfunder.netlify.com" className="nav-web-link">Go back to website</a>
         </div>
     )
 
